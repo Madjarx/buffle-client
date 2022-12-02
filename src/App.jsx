5c5ce6f27108f6984b5980272ddbcc7d8e38805a
Madjarx <lukaradisavljevic30@gmail.com>
2022-12-02 16:01:44 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd'
+
+function App() {
+  return (
+    
+    <>
+      <div>Hello from buffle</div>
+      <Button>Something</Button>
+    </>
+  );
+}
+
+export default App;
